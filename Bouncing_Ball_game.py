632ff655fcf195ddd7a5b9927ce7da29f9cf146a
Kamal Belbase <95961234+kbelbase45@users.noreply.github.com>
2022-07-11 01:46:09 -0700

    Add files via upload

--- Bouncing_Ball_game.py
+++ Bouncing_Ball_game.py
@@ -33,49 +33,87 @@ def constants():
 
 def window_configuration():
     x_min, y_min, x_max, y_max = constants()
-    canvas_window = GraphWin('Bouncing Ball',x_max,y_max)
+    canvas_window = GraphWin('Bouncing Ball by Kamal',x_max,y_max)
     canvas_window.setCoords(x_min,y_min,x_max,y_max)
     canvas_window.setBackground('gray')
     
     canvas_window.getMouse()
     
-    label = Text(Point(x_max/2,20),'Bouncing Ball by Kamal')
-    label.setTextColor('white')
+    label = Text(Point(380,20),'Two ball are bouncing')
+    label.setTextColor('green')
     label.setStyle('bold italic') 
     label.setFace('helvetica')    
-    label.setSize(25)             
+    label.setSize(20)             
     label.draw(canvas_window)         
     return canvas_window
 
-def get_random_center():
+def calculate_ranges_points(canvas_window,radius_of_ball):
     x_min, y_min, x_max, y_max = constants()
-    x = random.randrange(x_min,x_max+1)
-    y = random.randrange(y_min,y_max+1)
-    return Point(x,y)
+    
+    xLow  = radius_of_ball
+    yLow  = radius_of_ball
+    xHigh = canvas_window.getWidth() - radius_of_ball
+    yHigh = canvas_window.getHeight() - radius_of_ball
 
-def make_ball():
-    radius_of_ball = 10
-    center         = get_random_center() 
+    x = random.randrange(xLow,xHigh+1)
+    y = random.randrange(yLow,yHigh+1)
+    
+    return x,y
+
+def get_random_center(canvas_window):
+    radius_of_ball  = 10
+    radius_of_ball1 = radius_of_ball+3
+    
+    x,y   = calculate_ranges_points(canvas_window,radius_of_ball)        
+            
+    x1,y1 = calculate_ranges_points(canvas_window,radius_of_ball1)
+    
+    return Point(x,y), Point(x1,y1), radius_of_ball, radius_of_ball1
+
+def make_ball():    
     canvas_window  = window_configuration()
+    center,center1, radius_of_ball, radius_of_ball1  = get_random_center(canvas_window)     
     ball           = Circle(center,radius_of_ball)
+    ball1          = Circle(center1,radius_of_ball1)
+    
     ball.setOutline('red')
     ball.setFill('blue')
     ball.draw(canvas_window)
-    return ball
+    
+    ball1.setOutline('blue')
+    ball1.setFill('red')
+    ball1.draw(canvas_window)    
+    return ball,ball1, radius_of_ball, radius_of_ball1
 
 def make_ball_move(dx,dy):
-    ball = make_ball()
+    dx1,dy1 = dx+2,dy-2
+    ball,ball1, R, R1          = make_ball()
     x_min, y_min, x_max, y_max = constants()
-    delay = 0.002
-    for i in range(1000):
+    delay = 0.008
+    for i in range(2000):
         ball.move(dx,dy)
         center = ball.getCenter()
         x_now  = center.getX()
         y_now  = center.getY()
-        if x_now > x_max or x_now < x_min:
+                
+        if x_now > (x_max - R) or x_now < R:
             dx = -dx
-        if y_now > y_max or y_now < y_min:
+        if y_now > (y_max - R) or y_now < R:
             dy = -dy
+        
+        ball1.move(dx1,dy1)
+        center1 = ball1.getCenter()
+        x1_now  = center1.getX()
+        y1_now  = center1.getY()                
+
+        if x1_now > (x_max - R1 ) or x1_now < R1:
+            dx1 = -dx1
+        if y1_now > (y_max - R1 ) or y1_now < R1:
+            dy1 = -dy1
+        
+        if x_now == x1_now and y_now == y1_now:
+            print(' Case exist')
+        
         time.sleep(delay)  #delay is in second   
 
 
